2e4c7de111bf158d209eb26e6dc95be423611d0a
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-03-08 12:37:55 +0100

    json een eigen class geven

---
+++ Rotterdam Airlines/Rotterdam Airlines/JSON.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Rotterdam_Airlines
+{
+    class JSON
+    {
+        // DEFINE JSON PATHS
+        static string CustomersJSON = @"..\..\..\json\customers.json";
+        public static List<Customer> LoadCustomersJSON()
+        {
+            string jsonString = File.ReadAllText(CustomersJSON);
+            List<Customer> objects = JsonConvert.DeserializeObject<List<Customer>>(CustomersJSON);
+            return objects;
+        }
+        public static void SaveCustomersJSON(List<Customer> data)
+        {
+            string jsonString = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(CustomersJSON, jsonString);
+        }
+    }
+}
