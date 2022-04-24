7221b4b70d8c929ab717ebdd1977919831053764
Ant0n8 <a.shi2012@hotmail.com>
2022-04-24 21:14:00 +0200

    Bug fix

--- Rotterdam Airlines/Rotterdam Airlines/Admin.cs
+++ Rotterdam Airlines/Rotterdam Airlines/Admin.cs
@@ -775,7 +775,7 @@ namespace Rotterdam_Airlines
                             {
                                 Console.Clear();
                                 string screen = "Gebruiker Verwijderen";
-                                string question = "Wat is de email van de gebruiker die u wilt opzoeken?";
+                                string question = "Wat is de email van de gebruiker die u wilt verwijderen?";
                                 PrintAdminSubScreen(screen, question);
                                 if (Admin.RemoveUser())
                                 {
