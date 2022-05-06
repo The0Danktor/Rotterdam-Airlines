017c28e2dde4bf55a3a769f62a83067566e39c58
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-05-06 14:51:14 +0200

    Update Program.cs

--- Rotterdam Airlines/Rotterdam Airlines/Program.cs
+++ Rotterdam Airlines/Rotterdam Airlines/Program.cs
@@ -27,6 +27,7 @@ namespace Rotterdam_Airlines
             Customer CurrentUser = new Customer(null, null, null, null, null, null, null, null, null, null, new List<string>(), true);
             Admin AdminUser = new Admin("a", "a");
             //Admin AdminUser = new Admin("admin@rotterdamairlines.com", "321898aS*D*@ads-");
+            while(true)
             {
                 if (authorized)
                 {
